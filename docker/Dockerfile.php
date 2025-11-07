c9044a3ad5beaba257b05c113110059ea2a5c866
Houssem-nouri <h.nouri@one-solution.ad>
2025-11-07 13:05:39 +0100

    fix docker files

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -1,27 +1,37 @@
-# docker/Dockerfile.php
-
 FROM php:8.3-fpm-alpine
 
+# 1️⃣ Installer les dépendances système
 RUN apk update && apk add --no-cache \
     git \
     unzip \
     acl \
     mysql-client \
     && rm -rf /var/cache/apk/*
+
 RUN docker-php-ext-install pdo_mysql opcache
+
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
-WORKDIR /var/www/html
+COPY --chown=1000:1000 ./ /var/www/PriceManager
+
+WORKDIR /var/www/PriceManager
+
+COPY composer.json composer.lock ./
+
+RUN chmod -R 775 composer.json composer.lock ./
+
+COPY bin ./bin
+
+RUN composer install --optimize-autoloader --no-interaction --no-progress || true
+
 COPY . .
 
-USER root
+RUN mkdir -p /var/www/html/var /var/www/html/PriceManager \
+    && chown -R 1000:1000 /var/www/PriceManager \
+    && chmod -R 775 /var/www/PriceManager/var /var/www/PriceManager/vendor
 
-# RUN chown -R www-data:www-data /var/www/html/docker/db_setup.sh
+RUN chmod -R 775 composer.json composer.lock ./
 
-RUN composer install --optimize-autoloader
-
-RUN chown -R www-data:www-data /var/www/html
-
-USER www-data
+USER 1000:1000
 
 CMD ["php-fpm"]
